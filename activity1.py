323a6427e327f4997b66781d2d2b028d3483ceec
XxShayanxxX <balajishayan@gmail.com>
2024-11-27 20:18:55 +0800

    first commit

---
+++ activity1.py
@@ -0,0 +1,5 @@
+class Student : 
+    grade = 7 
+    print("Hi I am a student who studies in Dimensions , my grade is ", grade )
+
+object = Student()
---
+++ activity2/vehicle.py
@@ -0,0 +1,21 @@
+class Vehicle : 
+    def __init__(self,max_speed , milage):
+        self.max_speed = max_speed
+        self.milage = milage 
+
+mZ = Vehicle(240,20)
+mk = Vehicle(290,22)
+
+print("mZ speed : ",  mZ.max_speed)
+print("mk speed : ",  mk.max_speed)
+print("mZ speed : ",  mZ.milage)
+print("mk speed : ",  mk.milage)
+
+
+
+
+
+    
+
+
+    
